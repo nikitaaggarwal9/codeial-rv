b2947b1b64df24afae6b8607dd94583bffcea43b
Nikita <nikitaaggarwal6600@gmail.com>
2023-02-05 21:57:06 +0530

    Navbar with styling done

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { getPosts } from "../api";
 import { Home } from "../pages";
+// import { Loader } from "./";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log("response", response);
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
     };
 
     fetchPosts();
   }, []);
 
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Home posts={posts} />
     </div>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,5 @@
+const Loader = () => {
+  return <div className="app-spinner">Loading...</div>;
+};
+
+export default Loader;
