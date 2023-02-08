2f38adcd76b36039bce4492a9957d06a3421afa8
Nikita <nikitaaggarwal6600@gmail.com>
2023-02-08 13:25:41 +0530

    Login Page structure and styling done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import styles from '../styles/login.module.css'
 
 export default function Login() {
   return (
-    <h1>Login</h1>
-  )
+    <form className={styles.loginForm}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+
+      <div className={styles.field}>
+        <input type="email" placeholder="Email" required />
+      </div>
+
+      <div className={styles.field}>
+        <input type="password" placeholder="Password" required />
+      </div>
+
+      <div className={styles.field}>
+        <button>Log In</button>
+      </div>
+    </form>
+  );
 }
